c7d1f31f4a9dc21fee578ece717ebb56912e69d9
Josue Patricio <josretamozovar@gmail.com>
2025-01-15 20:59:16 -0500

    Add header component and update App layout with navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
 // escribo r + f  + c + e => rfce + enter para crear el componente
+
+import './App.css'
+import MiHeader from './components/mi-header';
+
 // jsx es una extension de javascript que permite escribir html en javascript
 function App() {
   return (
-    <div>
+    <>
+      <MiHeader/>
       <p>Hola mundo!! </p>
       <h1>Hola soy un titulo</h1>
-    </div>
+      <footer>
+        <nav>
+          <ul>
+            <li>Elemento 1</li>
+            <li>Elemento 2</li>
+            <li>Elemento 3</li>
+          </ul>
+          <ul>
+            <li>Contacto 1</li>
+            <li>Contacto 2</li>
+            <li>Contacto 3</li>
+          </ul>
+        </nav>
+      </footer>
+    </>
   );
 }
 
---
+++ src/components/mi-header.tsx
@@ -0,0 +1,25 @@
+// r + a + f + c + e -> rafce ->  arrow function export component (conponente flecha funcional react)
+
+const MiHeader = () => {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>Elemento 1</li>
+          <li>Elemento 2</li>
+          <li>Elemento 3</li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+// function BarraNavegacion({children}) {
+//   return (
+//     <nav>
+//       {children}
+//     </nav>
+//   )
+// }
+
+export default MiHeader;
